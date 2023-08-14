39e1847c5ec867b7ad1728ec568ace3a47d7bfaf
Walker Lobato <walker.b.lobato@gmail.com>
2023-08-14 10:59:04 -0300

    feat: create themes and component Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
-import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { ChakraProvider, CSSReset, Box } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+
+import { theme } from '@/themes'
+import { Header } from '@/components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <CSSReset />
-      <Component {...pageProps} />
+      <Box bg="background.black" height="100vh">
+        <Header />
+        <Component {...pageProps} />
+      </Box>
     </ChakraProvider>
   )
 }
