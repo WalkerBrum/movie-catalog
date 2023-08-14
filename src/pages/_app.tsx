0ae80ba056372d7d3f77bff0c9d38e16ffb3359b
My Name <walker.b.lobato@gmail.com>
2023-08-14 00:46:41 -0300

    feat: create project and inicial config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
