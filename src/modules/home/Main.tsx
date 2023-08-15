fdc50eac05ed7755d798bc79b1f3fdd8d8be880b
My Name <walker.b.lobato@gmail.com>
2023-08-15 01:46:44 -0300

    feat:  add populate  data in component Card with TMDB API

--- src/modules/home/Main.tsx
+++ src/modules/home/Main.tsx
@@ -1,9 +1,41 @@
-import { Box, Grid, GridItem } from '@chakra-ui/react'
+import { Box, Button, Grid } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 
 import { MovieCard } from './components/movie-card/MovieCard'
 import { SearchMovie } from './components/search-movie/SearchMovie'
+import { TmdbApi } from '@/services/api'
+
+interface IMovies {
+  adult: boolean
+  backdrop_path: string
+  genre_ids: number[]
+  id: number
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  release_date: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
 
 export const Main = () => {
+  const [page, setPage] = useState(1)
+  const [movies, setMovies] = useState<IMovies[]>([])
+
+  useEffect(() => {
+    const keyApi = `${process.env.NEXT_PUBLIC_API_KEY}`
+
+    TmdbApi.getPopularMovies(page, keyApi).then(({ data }) => {
+      setMovies(data.results)
+    })
+  }, [page])
+
+  console.log(movies)
+
   return (
     <Box p={[4, 7, 10]}>
       <SearchMovie />
@@ -12,28 +44,19 @@ export const Main = () => {
         gap={5}
         p={[4, 8, 10]}
       >
-        <GridItem style={{ justifySelf: 'center', alignSelf: 'center' }}>
-          <MovieCard />
-        </GridItem>
-        <GridItem style={{ justifySelf: 'center', alignSelf: 'center' }}>
-          <MovieCard />
-        </GridItem>
-        <GridItem style={{ justifySelf: 'center', alignSelf: 'center' }}>
-          <MovieCard />
-        </GridItem>
-        <GridItem style={{ justifySelf: 'center', alignSelf: 'center' }}>
-          <MovieCard />
-        </GridItem>
-        <GridItem style={{ justifySelf: 'center', alignSelf: 'center' }}>
-          <MovieCard />
-        </GridItem>
-        <GridItem style={{ justifySelf: 'center', alignSelf: 'center' }}>
-          <MovieCard />
-        </GridItem>
-        <GridItem style={{ justifySelf: 'center', alignSelf: 'center' }}>
-          <MovieCard />
-        </GridItem>
+        {movies.map((movie) => (
+          <MovieCard
+            title={movie.title}
+            imgUrl={movie.poster_path}
+            genreIds={movie.genre_ids}
+            key={movie.id}
+          />
+        ))}
       </Grid>
+
+      <Button onClick={() => setPage((prevState) => prevState + 1)}>
+        Próxima página
+      </Button>
     </Box>
   )
 }
--- src/modules/home/components/movie-card/MovieCard.tsx
+++ src/modules/home/components/movie-card/MovieCard.tsx
@@ -1,11 +1,45 @@
+import { TmdbApi } from '@/services/api'
 import { Flex, Card, Heading, Text } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 
-export const MovieCard = () => {
-  const cardImage = 'https://source.unsplash.com/random?b=1'
+interface IListGenres {
+  id: number
+  name: string
+}
+
+interface IMovieCardProps {
+  title: string
+  imgUrl: string
+  genreIds: number[]
+}
+
+export const MovieCard = ({ title, imgUrl, genreIds }: IMovieCardProps) => {
+  const [listGenres, setListGenres] = useState<IListGenres[]>([])
+  const [genres, setGenres] = useState<string[]>([])
+
+  useEffect(() => {
+    const keyApi = `${process.env.NEXT_PUBLIC_API_KEY}`
+
+    TmdbApi.getListGenres(keyApi).then(({ data }) => {
+      setListGenres(data.genres)
+      mapGenreIdsToNames(genreIds)
+    })
+  }, [genreIds])
+
+  const mapGenreIdsToNames = (genreIds: number[]): void => {
+    const mappedGenres = genreIds.map((id) => {
+      const matchedGenre = listGenres.find((genre) => genre.id === id)
+      return matchedGenre ? matchedGenre.name : ''
+    })
+
+    setGenres((prevState) => [...prevState, ...mappedGenres])
+  }
+
+  console.log(genres)
 
   return (
     <Card
-      bgImage={`url(${cardImage})`}
+      bgImage={`url(https://image.tmdb.org/t/p/w500${imgUrl})`}
       bgSize="cover"
       bgPosition="center"
       borderRadius="lg"
@@ -15,11 +49,18 @@ export const MovieCard = () => {
       color="base.gray100"
       cursor="pointer"
       p={5}
+      sx={{ justifySelf: 'center', alignSelf: 'center' }}
       _hover={{ filter: 'sepia(100%)' }}
     >
       <Flex direction="column" gap={2}>
-        <Heading as="h3" size="md" fontWeight="bold">Se Beber Não Case</Heading>
-        <Text fontWeight="bold">Comédia</Text>
+        <Heading as="h3" size="md" fontWeight="bold">
+          {title}
+        </Heading>
+        {genres.map((genre) => (
+          <Text fontWeight="bold" key={genre}>
+            {genre}
+          </Text>
+        ))}
         <Text fontWeight="bold">1h53min</Text>
       </Flex>
     </Card>
