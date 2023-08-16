c9813ab5fa328f6583692d5b257b7b5c8a0c30a7
Walker Lobato <walker.b.lobato@gmail.com>
2023-08-16 16:28:39 -0300

    refactor: Improving the look of the page

--- src/modules/movie/DetailMovie.tsx
+++ src/modules/movie/DetailMovie.tsx
@@ -9,6 +9,7 @@ import {
 } from 'react-icons/ai'
 import { BsFillBookmarkFill } from 'react-icons/bs'
 import { MoviesContext } from '@/context/MoviesContext'
+import { CircularIconWithProgressBar } from './components/circular-progress/CircularIconWithProgressBar'
 
 export const DetailMovie = () => {
   const { query } = useRouter()
@@ -55,47 +56,83 @@ export const DetailMovie = () => {
           width="100%"
           p={5}
         >
-          <CircularProgress value={progressValue} />
+          <CircularIconWithProgressBar progress={progressValue} />
           <AiOutlineHeart />
           <BsFillBookmarkFill></BsFillBookmarkFill>
           <AiFillStar></AiFillStar>
-          <Text>{infoMovie?.release_date}</Text>
+          <Text fontWeight="bolder" color="base.yellow500" fontSize={18}>
+            {infoMovie?.release_date}
+          </Text>
         </Flex>
 
         <Flex
           p={2}
           justifyContent="space-between"
+          alignItems="center"
           w="100%"
           border="2px solid white"
           borderRadius="lg"
         >
-          <Text alignSelf="flex-start" fontWeight="bold" as="span">
-            Conteúdo adulto: {infoMovie?.adult ? 'Sim' : 'Não'}
+          <Text fontWeight="bolder" color="base.yellow500" fontSize={18}>
+            Conteúdo adulto:{' '}
+            <Text
+              as="span"
+              color="base.gray100"
+              fontWeight="bold"
+              fontSize={16}
+            >
+              {infoMovie?.adult ? 'Sim' : 'Não'}
+            </Text>
           </Text>
-          {infoMovie?.genre_ids &&
-            mapGenreIdsToNames(infoMovie?.genre_ids).map((genre) => (
-              <Text
-                alignSelf="flex-start"
-                fontWeight="bold"
-                as="span"
-                key={genre.id}
-              >
-                Gênero: {genre.name}
-              </Text>
-            ))}
+          <Flex justify="center" gap={5} alignItems="center">
+            <Text fontWeight="bolder" color="base.yellow500" fontSize={18}>
+              Gênero:
+            </Text>
+            {infoMovie?.genre_ids &&
+              mapGenreIdsToNames(infoMovie?.genre_ids).map((genre) => (
+                <Text
+                  alignSelf="flex-start"
+                  fontWeight="bold"
+                  as="span"
+                  key={genre.id}
+                  bg={`geners.${genre.id}`}
+                  p={2}
+                  borderRadius={5}
+                  fontSize={16}
+                  color="base.gray100"
+                >
+                  {genre.name}
+                </Text>
+              ))}
+          </Flex>
         </Flex>
 
-        <Flex>
-          <Text fontWeight="bold" as="span" p="5">
-            Descrição:
+        <Flex margin="20px 0">
+          <Text fontWeight="bolder" color="base.yellow500" fontSize={18} p={3}>
+            Sinopse:
           </Text>
-          <Text fontWeight="normal" as="span" p="5" maxW="800px">
+          <Text
+            fontWeight="bold"
+            p={3}
+            fontSize={16}
+            maxW={700}
+            color="base.gray100"
+          >
             {infoMovie?.overview}
           </Text>
         </Flex>
 
-        <Text p={2} alignSelf="flex-start" fontWeight="bold" as="span">
-          Título original: {infoMovie?.original_title}
+        <Text
+          p={2}
+          alignSelf="flex-start"
+          fontWeight="bolder"
+          color="base.yellow500"
+          fontSize={18}
+        >
+          Título original:{' '}
+          <Text as="span" fontWeight="bold" color="base.gray100" fontSize={16}>
+            {infoMovie?.original_title}
+          </Text>
         </Text>
 
         <Flex
@@ -108,11 +145,39 @@ export const DetailMovie = () => {
           border="2px solid white"
           borderRadius="lg"
         >
-          <Text p={2} alignSelf="flex-start" fontWeight="bold" as="span">
-            Popularidade: {infoMovie?.popularity}
+          <Text
+            p={2}
+            alignSelf="flex-start"
+            fontWeight="bolder"
+            color="base.yellow500"
+            fontSize={18}
+          >
+            Popularidade:{' '}
+            <Text
+              fontWeight="bold"
+              fontSize={16}
+              color="base.gray100"
+              as="span"
+            >
+              {infoMovie?.popularity}
+            </Text>
           </Text>
-          <Text p={2} alignSelf="flex-start" fontWeight="bold" as="span">
-            Votos: {infoMovie?.vote_average}
+          <Text
+            p={2}
+            alignSelf="flex-start"
+            fontWeight="bolder"
+            color="base.yellow500"
+            fontSize={18}
+          >
+            Votos:{' '}
+            <Text
+              fontWeight="bold"
+              fontSize={16}
+              color="base.gray100"
+              as="span"
+            >
+              {infoMovie?.vote_average}
+            </Text>
           </Text>
         </Flex>
       </Flex>
---
+++ src/modules/movie/components/circular-progress/CircularIconWithProgressBar.tsx
@@ -0,0 +1,31 @@
+import { Box, Card, Flex, Text, CircularProgress } from '@chakra-ui/react'
+
+interface ICircularIconWithProgressBarProps {
+  progress: number
+}
+
+export const CircularIconWithProgressBar = ({
+  progress,
+}: ICircularIconWithProgressBarProps) => {
+  return (
+    <Box position="relative" display="inline-block">
+      <CircularProgress
+        value={progress}
+        size="72px"
+        thickness="10px"
+        color="base.yellow500"
+        trackColor="base.gray100"
+      />
+      <Box
+        position="absolute"
+        top="47%"
+        left="50%"
+        transform="translate(-50%, -50%)"
+        fontWeight="bold"
+        fontSize={30}
+      >
+        {progress}
+      </Box>
+    </Box>
+  )
+}
